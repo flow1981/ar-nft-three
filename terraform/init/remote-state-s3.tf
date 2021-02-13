4a187c12a7056a92f14ef2404a8bee31244dff86
flow1981 <florian.stagliano@gmx.de>
2021-02-13 14:44:46 +0000

    Fixes setup of terraform state backend

--- terraform/init/remote-state-s3.tf
+++ terraform/init/remote-state-s3.tf
@@ -1,9 +1,12 @@
+//used to initialize the remote backend for the environment
+
 provider "aws" {
   region = "eu-west-3"
 }
 
 resource "aws_s3_bucket" "terraform-state-backend" {
     bucket = "terraform-state-ar-nft-three"
+    acl    = "private"
     versioning {
       enabled = true
     }
