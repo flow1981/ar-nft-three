ca4611083499348cb49498762897bafccf197c2d
flow1981 <florian.stagliano@gmx.de>
2021-02-16 23:19:17 +0000

    Cleanup index.js

--- app/index.js
+++ app/index.js
@@ -15,91 +15,86 @@ const earthRadius = 6371
 window.AROnLoad = function(tle) {
 
 	ARController.getUserMediaThreeScene({maxARVideoSize: 320, cameraParam: CAMERA_PARAM_URL,
-	onSuccess: function(arScene, arController, arCamera) {
+		onSuccess: function(arScene, arController, arCamera) {
 
-		document.body.className = arController.orientation;
+			document.body.className = arController.orientation;
 
-		//Set up ambient light source
-		let ambientLight = new THREE.AmbientLight( 0xcccccc, 0.8 );
-		arScene.scene.add( ambientLight );
+			let ambientLight = new THREE.AmbientLight( 0xcccccc, 0.8 );
+			arScene.scene.add( ambientLight );
 
+			let renderer = new THREE.WebGLRenderer({antialias: true});
+			renderer.gammaOutput = true;
+			renderer.gammaFactor = 2.2;
 
-		// Set up renderer
-		let renderer = new THREE.WebGLRenderer({antialias: true});
-		renderer.gammaOutput = true;
-		renderer.gammaFactor = 2.2;
-
-		if (arController.orientation === 'portrait') {
-			let w = (window.innerWidth / arController.videoHeight) * arController.videoWidth;
-			let h = window.innerWidth;
-			renderer.setSize(w, h);
-			renderer.domElement.style.paddingBottom = (w-h) + 'px';
-		} else {
-			if (/Android|mobile|iPad|iPhone/i.test(navigator.userAgent)) {
-				renderer.setSize(window.innerWidth, (window.innerWidth / arController.videoWidth) * arController.videoHeight);
+			if (arController.orientation === 'portrait') {
+				let w = (window.innerWidth / arController.videoHeight) * arController.videoWidth;
+				let h = window.innerWidth;
+				renderer.setSize(w, h);
+				renderer.domElement.style.paddingBottom = (w-h) + 'px';
 			} else {
-				renderer.setSize(arController.videoWidth, arController.videoHeight);
-				document.body.className += ' desktop';
+				if (/Android|mobile|iPad|iPhone/i.test(navigator.userAgent)) {
+					renderer.setSize(window.innerWidth, (window.innerWidth / arController.videoWidth) * arController.videoHeight);
+				} else {
+					renderer.setSize(arController.videoWidth, arController.videoHeight);
+					document.body.className += ' desktop';
+				}
 			}
+
+			document.body.insertBefore(renderer.domElement, document.body.firstChild);
+
+			// renderer.domElement.addEventListener('click', function(ev) {
+			// 	ev.preventDefault();
+			// 	rotationTarget += 1;
+			// }, false);
+
+			let modelGroup = new THREE.Group();
+			// x positive - left, y positive - up, z positive -towards viewer | x, y zero is bottom right of trigger
+			modelGroup.applyMatrix(new THREE.Matrix4().makeScale(-1, 1, 1)); // we need flip the objects since ARtoolkit displays them mirrored
+			modelGroup.position.set(80,80,80)
+
+			let earth = createEarthGnonomic(earthRadius, scaleFactor)
+			earth = alignXeciToVernalEquinox(earth)
+			modelGroup.add(earth)	
+			
+			let issPosition = createIssPositionMarker()
+			issPosition.scale.set(200,200,200);
+			issPosition = initOrbitalPosition(issPosition, tle, 0, scaleFactor)
+			issPosition = updateOrbitalPostion(issPosition, scaleFactor)
+			modelGroup.add(issPosition)
+			
+			let orbit = visualizeOrbit(issPosition.userData.satrec, scaleFactor)
+			modelGroup.add(orbit)
+
+			addIssModelToMarker(issPosition, ISS_MODEL_URL)
+			alignISSrelativeEarthSurface(issPosition)
+
+			modelGroup.rotateOnAxis( new THREE.Vector3(1, 0, 0).normalize(), 90 * Math.PI/180 );
+
+			arController.loadNFTMarker(NFT_MARKER_URL, function(markerId) {
+				let markerRoot = arController.createThreeNFTMarker(markerId);
+
+				arScene.scene.add(markerRoot);
+				markerRoot.add(modelGroup);
+			});
+
+			// var rotationV = 0;
+			// var rotationTarget = 0;
+
+			const animate = function() {
+				arScene.process();
+				// rotationV += (rotationTarget - sphere.rotation.z) * 0.05;
+				// sphere.rotation.z += rotationV;
+				// rotationV *= 0.8;
+
+				arScene.renderOn(renderer);
+				requestAnimationFrame(animate);
+			};
+
+			animate();
 		}
+	});
 
-		document.body.insertBefore(renderer.domElement, document.body.firstChild);
-
-		// renderer.domElement.addEventListener('click', function(ev) {
-		// 	ev.preventDefault();
-		// 	rotationTarget += 1;
-		// }, false);
-
-		let modelGroup = new THREE.Group();
-		// x positive - left, y positive - up, z positive -towards viewer | x, y zero is bottom right of trigger
-		modelGroup.applyMatrix(new THREE.Matrix4().makeScale(-1, 1, 1)); // we need flip the objects since ARtoolkit displays them mirrored
-		modelGroup.position.set(80,80,80)
-
-		// Add Three.js models
-		let earth = createEarthGnonomic(earthRadius, scaleFactor)
-		earth = alignXeciToVernalEquinox(earth)
-		modelGroup.add(earth)	
-		
-		let issPosition = createIssPositionMarker()
-		issPosition.scale.set(200,200,200);
-		issPosition = initOrbitalPosition(issPosition, tle, 0, scaleFactor)
-		issPosition = updateOrbitalPostion(issPosition, scaleFactor)
-		modelGroup.add(issPosition)
-		
-		let orbit = visualizeOrbit(issPosition.userData.satrec, scaleFactor)
-		modelGroup.add(orbit)
-
-		addIssModelToMarker(issPosition, ISS_MODEL_URL)
-		alignISSrelativeEarthSurface(issPosition)
-
-		modelGroup.rotateOnAxis( new THREE.Vector3(1, 0, 0).normalize(), 90 * Math.PI/180 );
-
-		// Create NFT marker and associate it with Three.js model group
-		arController.loadNFTMarker(NFT_MARKER_URL, function(markerId) {
-			let markerRoot = arController.createThreeNFTMarker(markerId);
-
-			arScene.scene.add(markerRoot);
-			markerRoot.add(modelGroup); //Link models with NFT marker
-		});
-
-		// var rotationV = 0;
-		// var rotationTarget = 0;
-
-		const animate = function() {
-			arScene.process();
-			// rotationV += (rotationTarget - sphere.rotation.z) * 0.05;
-			// sphere.rotation.z += rotationV;
-			// rotationV *= 0.8;
-
-			arScene.renderOn(renderer);
-			requestAnimationFrame(animate);
-		};
-
-		animate();
-
-	}});
-
-	delete window.ARThreeOnLoad;
+	delete window.AROnLoad;
 
 };
 
